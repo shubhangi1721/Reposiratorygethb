b70e14f38ee1a2bfef94ccca275b8795c874d75d
Ajit <Ajit@DESKTOP-AF5KDQ8>
2021-04-11 18:34:56 +0530

    [Ajit]: adding project to gethub

---
+++ src/demo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Ajit
+ *
+ */
+package demo;
---
+++ src/demo/sample1.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class sample1 
+{
+public static void main(String[] args) {
+	System.out.println("hiii");
+}
+}
